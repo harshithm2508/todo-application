faa8860957d8523d2e3939f6bb8d802b64ccf7c7
harshithm2508 <harshithm2508@gmail.com>
2024-12-24 00:48:37 +0530

    email_unique DB migration

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
 import { auth } from "@/auth"
+import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/db";
 
 export default async function Home() {
   const session = await auth();
   return(
     <div className="">
-      {(session!=null) ? (JSON.stringify(session,null,2)) : "Landing Page"}
+      {(session!=null) ? (JSON.stringify(session,null,2)) : (
+        <div>
+          Landing Page
+        </div>
+      )}
       <div>
         <img src={session?.user?.image!} alt="" />
       </div>
     </div>
   )
 }
+
+
+
+//  For deleting complete user data : 
+/*
+  <form action={
+            async () => {
+              "use server"
+              await prisma.user.deleteMany();
+            }
+          }>
+            <Button className=" ml-5 mt-5" type="submit">
+              Delete data
+            </Button>
+          </form>  
+*/

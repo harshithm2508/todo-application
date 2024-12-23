e5ebb5a875a4d91e1a5d966715857d7620d34ac9
harshithm2508 <harshithm2508@gmail.com>
2024-12-24 00:06:35 +0530

    Set signIn process

--- auth.ts
+++ auth.ts
@@ -1,6 +1,34 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google"
+import { prisma } from '@/lib/db'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers : [Google],
+    callbacks : {
+        async signIn({account, credentials, email, profile, user}) {
+            
+            try {
+                
+                const dataFeed = await prisma.user.create({
+                    data : {
+                        userId : user.id!,
+                        name : user.name!,
+                        firstName : profile?.given_name!,
+                        lastName : profile?.family_name,
+                        email : user.email!,
+                        picture : user.image!
+                    }
+                })
+
+                console.log(` User with id : ${dataFeed.userId} `)
+
+            } catch(err) {
+                console.log("There was an error in creating the user");
+                console.log(err);
+            }
+
+
+            return true;
+        }
+    }
 })
